9891e8c6d4e2ec841cb4782c3ae654e537caffea
Mohsen Hadianfard <mhadianfard@sweettoothhq.com>
2016-04-20 19:46:04 -0400

    Got basic sockets and spawn to work

--- index.js
+++ index.js
@@ -1,14 +1,71 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const childProcess = require('child_process');
+var spawn = childProcess.spawn;
 
-app.use(express.static('public'));
+var OfficeCast = {
+    casts: {},
+};
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+
+
+/**
+ * Socket Setup
+ */
+io.on('connection', function(socket) {
+    console.log('Client Connected');
+
+    socket.on('disconnect', function() {
+       console.log('Client Disconnected');
+    });
+
+    socket.on('new cast', function(cast) {
+        console.log('New Cast: ', cast.shortName);
+        cast.process = null;
+        OfficeCast.casts[cast.id] = cast;
+    });
+
+    socket.on('start cast', function(data) {
+        var cast = OfficeCast.casts[data.id];
+        if (!cast) {
+            socket.emit('cast error', 'Specified cast ('+data.id+') does not exist');
+            return;
+        }
+        cast.url = data.url;
+        cast.process = spawn('node', ['cli/cast', cast.shortName, cast.url]);
+        cast.process.stdout.on('data', function(data) {
+            socket.emit('cast output', {
+                id: cast.id,
+                message: String.fromCharCode.apply(null, data),
+                type: 'stdout'
+            });
+        });
+        cast.process.stderr.on('data', function(data) {
+            socket.emit('cast output', {
+                id: cast.id,
+                message: String.fromCharCode.apply(null, data),
+                type: 'stderr'
+            });
+        });
+        cast.process.on('close', function(code) {
+            socket.emit('cast close', {
+                id: cast.id,
+                code: code
+            });
+        });
+    });
 });
 
+
+
+
+
+/**
+ * Server
+ */
+app.use(express.static('public'));
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,29 +1,74 @@
-(function($){
+(function($) {
+    var socket = io();
+
+    /**
+     * Cast Object
+     * @param id
+     * @param shortName
+     * @param url
+     * @returns {{id: *, shortName: *, url: *, isStarted: boolean}}
+     * @constructor
+     */
+    var Cast = function(id, shortName, url) {
+        var cast = {
+            id: id,
+            shortName: shortName,
+            url: url,
+            isStarted: false,
+
+            start: function(url)
+            {
+                if (url) {
+                    this.url = url;
+                }
+                if (!this.url) {
+                    alert('No URL specified.');
+                    return false;
+                }
+                socket.emit('start cast', {
+                    id:     this.id,
+                    url:    this.url
+                });
+            },
+
+            publish: function(message, messageType)
+            {
+                messageType = !messageType? 'notice' : messageType;
+                var logs = this.dom.find('.log');
+                logs.append('<div class="' + messageType + '">' + message + '</div>');
+            }
+        }
+
+        window.OfficeCast.casts[cast.id] = cast;
+        return cast;
+    };
+
+    /**
+     * OfficeCast App
+     */
     window.OfficeCast =
     {
         casts: {},
 
         addCast: function(castName, castUrl)
         {
+            var id = Object.keys(this.casts).length;
+            var cast = new Cast(id, castName, castUrl);
             var template = this._loadTemplateHtml('cast');
-            var dom = $(template);
+            cast.dom = $(template);
 
-            $('.casts').append(dom);
-            dom.find('.title').html(castName);
+            $('.casts').append(cast.dom);
+            cast.dom.find('.title').html(castName);
             if (castUrl) {
-                dom.find('input.url').val(castUrl);
-                dom.find('.control.start').removeClass('disabled');
+                cast.dom.find('input.url').val(castUrl);
+                cast.dom.find('.control.start').removeClass('disabled');
             }
 
-            var cast = {
-                id: Object.keys(this.casts).length,
-                shortName: castName,
-                url: castUrl,
-                dom: dom,
-                isStarted: false,
-            };
+            cast.dom.find('.control.start').click(function(){
+               cast.start();
+            });
 
-            this.casts[cast.id] = cast;
+            socket.emit('new cast', cast);
             return cast;
         },
 
@@ -32,4 +77,40 @@
             return $('script#template-' + templateName).html();
         }
     };
+
+    /**
+     * Socket Setup
+     */
+    socket.on('connect', function() {
+        $('.connection').removeClass('offline').addClass('online');
+        $('.connection .status').html('connected');
+    });
+
+    socket.on('disconnect', function() {
+        $('.connection').removeClass('online').addClass('offline');
+        $('.connection .status').html('disconnected');
+        socket.on('connect', function() {
+            location.reload();
+        });
+    });
+
+    socket.on('cast error', function(message) {
+        alert("[FROM SERVER]:\n\n" + message);
+    });
+
+    socket.on('cast output', function(data) {
+        var cast = OfficeCast.casts[data.id];
+        if (!cast) {
+            alert('unrecognized cast id: ' + data.id);
+        }
+        cast.publish(data.message);
+    });
+
+
+    /**
+     * Execute on page load
+     */
+    $(function() {
+
+    });
 })(jQuery);
