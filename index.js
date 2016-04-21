9948b7813ca50e0fbca41c890cfbb28aa59cfa2a
Mohsen Hadianfard <mhadianfard@sweettoothhq.com>
2016-04-21 17:49:47 -0400

    changing server to port 80

--- index.js
+++ index.js
@@ -152,6 +152,6 @@ app.get('/casts', function(request, response) {
     response.send(data);
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(80, function() {
+    console.log('listening on *:80');
 });
